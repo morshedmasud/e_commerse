9c3acec23682e8a2608d64374d9a540ac55ccc08
Masud Morshed <masudraj6@gmail.com>
2019-02-12 19:07:41 +0600

    place order section complete

--- orders/forms.py
+++ orders/forms.py
@@ -6,3 +6,11 @@ class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'postal_code': forms.TextInput(attrs={'class': 'form-control'}),
+            'city': forms.TextInput(attrs={'class': 'form-control'})
+        }
--- orders/urls.py
+++ orders/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('create/', views.order_create, name='order_create')
+    path('create/', views.order_create, name='order_create'),
 ]
